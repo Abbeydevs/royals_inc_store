63a3cd14ca70b49445c22c51fb071fe7a443802f
abbeydevs <abbey.developer@gmail.com>
2024-01-03 14:34:20 +0100

    fix category issue

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -37,7 +37,6 @@ const HomePage: React.FC<HomePageProps> = async ({ params, searchParams }) => {
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
-
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <Filter valueKey="sizeId" name="Sizes" data={sizes} />
           <Filter valueKey="colorId" name="Colors" data={colors} />
